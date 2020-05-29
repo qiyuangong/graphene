5509a9f8bd710e5651d4b61cddf2b17f029c6238
Qiyuan Gong <qiyuan.gong@intel.com>
2020-05-30 02:23:25 +0800

    Mini

--- Examples/ray/scripts/test-ray.py
+++ Examples/ray/scripts/test-ray.py
@@ -4,11 +4,11 @@ import ray
 
 ray.init(memory=60 * 1024 * 1024,
          object_store_memory=75 * 1024 * 1024,
-         driver_object_store_memory=5 * 1024 * 1024)
+         driver_object_store_memory=15 * 1024 * 1024)
 
 @ray.remote
 def f(x):
     return x * x
 
-futures = [f.remote(i) for i in range(1)]
+futures = [f.remote(i) for i in range(0)]
 print(ray.get(futures))
--- Examples/ray/test.sh
+++ Examples/ray/test.sh
@@ -1,6 +1,10 @@
 make clean
 cp ../gcc/*.sig .
 cp ../gcc/*.sgx .
-make PYTHONPATH=/usr PYTHONVERSION=python3.6 SGX=1
+cp ../gcc/*.token .
+cp ../redis/*.sgx .
+cp ../redis/*.sig .
+cp ../redis/*.token .
+make DEBUG=1 PYTHONPATH=/usr PYTHONVERSION=python3.6 SGX=1
 cp python.manifest.sgx python3.6.manifest.sgx
-SGX=1 ./pal_loader python.manifest scripts/test-ray.py
+#SGX=1 ./pal_loader python.manifest scripts/test-ray.py
