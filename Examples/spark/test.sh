96b854ffa386eaac97d26777742ae50d4a4ea374
Qiyuan Gong <qiyuan.gong@intel.com>
2020-07-14 09:31:49 +0800

    Master & slave work well

--- Examples/spark/test.sh
+++ Examples/spark/test.sh
@@ -1,6 +1,9 @@
 make clean
-make SGX=1 DEBUG=1
+#make SGX=1 DEBUG=1 -j20
+make SGX=1 -j20
 #./pal_loader bash.manifest -c "/bin/bash -l ./scripts/spark-2.4.3-bin-hadoop2.7/sbin/spark-daemon.sh start org.apache.spark.deploy.master.Master 1 --host localhost --port 7077 --webui-port 8080"
 #./pal_loader bash.manifest -c "/bin/bash -l ./scripts/spark-2.4.3-bin-hadoop2.7/bin/spark-class org.apache.spark.deploy.master.Master --host localhost --port 7077 --webui-port 8080"
-./pal_loader bash.manifest -c "java -cp /home/qiyuan/working/graphene/Examples/spark/scripts/spark-2.4.3-bin-hadoop2.7/conf/:/home/qiyuan/working/graphene/Examples/spark/scripts/spark-2.4.3-bin-hadoop2.7/jars/\* -Xmx1g org.apache.spark.deploy.master.Master --host localhost --port 7077 --webui-port 8080"
+#./pal_loader bash.manifest -c "ls"
+#./pal_loader bash.manifest -c "java -classpath ./scripts/spark-2.4.3-bin-hadoop2.7/conf/:./scripts/spark-2.4.3-bin-hadoop2.7/jars/* -Xmx1g org.apache.spark.deploy.master.Master --host 10.239.166.29 --port 7077 --webui-port 8080"
+./pal_loader bash.manifest -c "java -classpath ./scripts/spark-2.4.3-bin-hadoop2.7/conf/:./scripts/spark-2.4.3-bin-hadoop2.7/jars/* -Xmx1g org.apache.spark.deploy.master.Master --host 10.239.166.29 --port 7077 --webui-port 8080"
 #./pal_loader bash.manifest -c "cd scripts && bash test_spark.sh"
